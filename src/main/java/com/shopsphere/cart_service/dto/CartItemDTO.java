3706ad26e11fb32c9c9ddd977e0ade76bbda5630
Yasura Lakshitha <lakshitha200087@gmail.com>
2025-05-27 09:49:40 +0530

    formated DTO objects

--- src/main/java/com/shopsphere/cart_service/dto/CartItemDTO.java
+++ src/main/java/com/shopsphere/cart_service/dto/CartItemDTO.java
@@ -1,5 +1,8 @@
 package com.shopsphere.cart_service.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Builder;
 import lombok.Data;
 
@@ -7,9 +10,14 @@ import lombok.Data;
 @Builder
 public class CartItemDTO {
 
+    @NotEmpty(message = "Item name is required")
     private String itemName;
 
+    @NotNull(message = "Item quantity is required")
+    @Positive(message = "Item quantity should be positive")
     private Integer itemQuantity;
 
+    @NotNull(message = "Item price is required")
+    @Positive(message = "Item price should be positive")
     private Double totalItemPrice;
 }
